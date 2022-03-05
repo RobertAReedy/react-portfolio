6c2f7a29766146dd8afbdacdc96085bf6ac41f34
RobertAReedy <robertandreedy@gmail.com>
2022-03-05 17:31:16 -0500

    CSS file split into separate ones in component folders

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./aboutMe.css";
 
 const brandStatement = "Back-end developer interested in gaining a more thorough understanding of the broader spectrum of programming. Graduated with a certificate in  Full Stack Web Development from the University of Richmond, gaining skills and familiarity with HTML, CSS, Javascript, and various other tools such as React, SQL, and Mongoose. Known for focusing on details such as refactoring optimization, performance improvement, and security vulnerabilities. Recently worked on a team of four to produce a full stack MERN application that allows a user to search for books based on movies they enjoyed, or vise versa. Most of all, though: itching to get into the field to further improve skills, knowledge, and understanding of the programming landscape.";
 

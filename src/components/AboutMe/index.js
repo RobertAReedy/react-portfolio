0bbf5c8692dd69923c81732a8535fa06c8c455ba
RobertAReedy <robertandreedy@gmail.com>
2022-03-04 19:22:54 -0500

    tabs now change color on hover and change mouse pointer

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 
+const brandStatement = "Back-end developer interested in gaining a more thorough understanding of the broader spectrum of programming. Graduated with a certificate in  Full Stack Web Development from the University of Richmond, gaining skills and familiarity with HTML, CSS, Javascript, and various other tools such as React, SQL, and Mongoose. Known for focusing on details such as refactoring optimization, performance improvement, and security vulnerabilities. Recently worked on a team of four to produce a full stack MERN application that allows a user to search for books based on movies they enjoyed, or vise versa. Most of all, though: itching to get into the field to further improve skills, knowledge, and understanding of the programming landscape.";
+
 function AboutMe() {
   return (
     <section class="about-me" id="about-me">
       <h2 class="header-text">About Me</h2>
-      <p class="about-me-p">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi nihil sunt explicabo voluptate sapiente, reprehenderit aspernatur voluptatibus nulla voluptates eum omnis suscipit blanditiis voluptas quidem. Incidunt suscipit aut ipsa quam?
-      </p>
-      <p class="about-me-p">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi nihil sunt explicabo voluptate sapiente, reprehenderit aspernatur voluptatibus nulla voluptates eum omnis suscipit blanditiis voluptas quidem. Incidunt suscipit aut ipsa quam?
-      </p>
+      <p class="about-me-p">{brandStatement}</p>
+      
     </section>
   );
 }
